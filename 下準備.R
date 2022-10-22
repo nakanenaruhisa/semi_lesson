1bf2892550ea501f6d5cf0b9bc6ad31a58d67cc6
nakanenaruhisa <104552238+nakanenaruhisa@users.noreply.github.com>
2022-10-22 11:02:04 +0900

    Add files via upload

---
+++ 下準備.R
@@ -0,0 +1,25 @@
+#Rを使う下準備
+#packageの準備
+library(tidyverse)
+library(ggplot2)
+library(GGally)
+library(dbplyr)
+library(ggthemes)
+library(gtsummary)
+
+#全部の変数を消す
+rm(list=ls())
+
+#テーマのセット
+theme_set(theme_gray(
+  base_family = "HiraginoSans-W3",# macOS用
+  base_size = 11, #文字の大きさを設定。デフォルトは11
+  base_line_size = 0.2, #罫線の線の太さを設定。デフォルトはbase_size/22
+  base_rect_size = 0.2 #外枠の線の太さを設定。デフォルトはbase_size/22
+))
+ 
+#theme_set(theme_gray(base_size = 10, base_family = "Meiryo"))        # Windows用
+#theme_set(theme_gray(base_size = 10, base_family = "IPAGothic"))     # Ubuntu用
+#showtext::showtext_auto()                                            # Cloud用
+#theme_set(theme_gray(base_size = 10, base_family = "noto"))          # Cloud用
+
