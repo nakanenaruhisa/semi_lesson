a7f9853d0fd180bcc2ac834799cd2dcb100797d3
nakanenaruhisa <104552238+nakanenaruhisa@users.noreply.github.com>
2023-04-19 15:20:11 +0900

    Update code100.R

--- height/code100.R
+++ height/code100.R
@@ -22,147 +22,59 @@ theme_set(theme_gray(
 rm(list=ls())
 
 #datasetを読み込む
-ten_kukan100 <- read_csv("height/testdataset.csv")
 
-
-
-ten_kukan100 <-ten_kukan100 %>%
-  mutate(sex_c =
-           case_when(sex == "Male" ~ "男性",
-                     sex == "Female" ~ "女性",
-                     TRUE ~ sex)) 
 #変数を箱に入れるよ
-height <- (ten_kukan100$height)
-weight <- (ten_kukan100$weight)
-sex_c <- (ten_kukan100$sex_c)
 
-view(ten_kukan100)
 
 #身長の平均値を出してみて
-mean(height)
+
 #体重の平均値を出してみて
-mean(weight)
+
 #男性の身長の平均値を出してみて
-maleheightmean <- subset(height,sex_c == "男性")
-maleheightmean <- mean(maleheightmean) 
-print(maleheightmean)
+
 
 #女性の身長の平均値を出してみて
-femaleheightmean <- subset(height,sex_c == "女性")
-femaleheightmean <- mean(femaleheightmean)
-print(femaleheightmean)
+
 
 #身長の標準偏差出してみて
-sd(height)
+
 #体重の標準偏差出してみて
-sd(weight)
+
 
 #標準誤差を出すためにパッケージを追加する。
-install.packages("plotrix")
-library(plotrix)
+
 
 #身長の標準誤差を出す
-std.error(height)
+
 
 #体重の標準誤差を出す
-std.error(weight)
+
 
 #男性の身長の標準偏差出してみて
-maleheightsd <- subset(height,sex_c == "男性")
-maleheightsd <- sd(maleheightsd)
-print(maleheightsd)
+
 
 #女性の身長の標準偏差出してみて
-femaleheightsd <- subset(height,sex_c == "女性") 
-femaleheightsd <- sd(femaleheightsd)
-print(femaleheightsd)
+
 
 #データセット全体のテーブルを自動でつくる
-ten_kukan100 %>% 
-  tbl_summary()
+
 
 #記述統計量にラベルを付けてテーブルで出力する
-ten_kukan100 %>% 
-  select(height,weight,sex_c) %>% 
-  tbl_summary(label = list(sex_c ~ "性別",
-                           height ~ "身長",
-                           weight ~ "体重"), #~の前には列名、後ろにはつけたい名前を""で囲んで入れ、,で一つずつ区切る
-              statistic = list(all_continuous() ~ "{mean} ({sd})"), 
-              digits = all_continuous() ~ 1) %>% #数値の部分が小数点第y位の部分の値
-  modify_header(label ~ "身長と体重テーブル") # ""の部分には好きな文字列を入れられる。何も入れなければ空欄になる
+
 
 #せっかくだから男女別に書いてみる
-ten_kukan100 %>% 
-  select(height,weight,sex_c) %>% 
-  tbl_summary(label = list(sex_c ~ "性別",
-                           height ~ "身長",
-                           weight ~ "体重"), #~の前には列名、後ろにはつけたい名前を""で囲んで入れ、,で一つずつ区切る
-              statistic = list(all_continuous() ~ "{mean} ({sd})"),
-              by = sex_c,
-              digits = all_continuous() ~ 1) %>% #数値の部分が小数点第y位の部分の値
-  modify_header(label ~ "男女別身長体重") # ""の部分には好きな文字列を入れられる。何も入れなければ空欄になる
 
 #身長と体重のプロット書いてみて
-ggplot(data = ten_kukan100) +     # tenkukan100データでキャンバス準備
-  aes(x = height, y = weight)+ # height,weight列をx,y軸にmapping
-  geom_point() +                  # 散布図を描く
-  scale_colour_tableau()+
-  theme_gray(base_size = 15) + #grayテーマで
-  theme_gray(base_family = "HiraKakuPro-W3") #文字化けしないおまじない
+
 
 #身長と体重の性別ごとのプロット書いてみて
-ggplot(data = ten_kukan100) +     # tenkukan100データでキャンバス準備
-  aes(x = height, y = weight, colour = sex_c)+ # height,weight列をx,y軸にmapping,sexごとに色分け
-  geom_point() +                  # 散布図を描く
-  scale_colour_tableau()+
-  theme_gray(base_size = 15) + #grayテーマで
-  theme_gray(base_family = "HiraKakuPro-W3") #文字化けしないおまじない
+
 
 #男性と女性の身長の箱ひげ図を書いて比較してみて
-ggplot(data = ten_kukan100) +     # tenkukan100データでキャンバス準備
-  aes(x = sex_c, y = height, fill = sex_c)+ # height,weight列をx,y軸にmapping,sexごとに色分け
-  geom_boxplot() +                  # はこひげ図を描く
-  xlab("sex") + ylab("height") +
-  coord_flip()+ 
-  scale_colour_tableau()+
-  theme_gray(base_size = 15) + #grayテーマで
-  theme_gray(base_family = "HiraKakuPro-W3") #文字化けしないおまじない
+
 
 #男性と女性の身長の平均の比較をseエラーバーをつけて書く（ggplot）
-ggplot(data = ten_kukan100)+
-  aes(x = sex_c, y = height, color = sex_c)+
-  stat_summary(aes(x = sex_c),fun = "mean", geom = "point", size = 3)+　#平均値のプロット
-  stat_summary(fun.data = "mean_se", geom = "errorbar", width = 0.1, lwd = 1)+
-  xlab("sex") + ylab("height") +
-  scale_colour_tableau()+
-  coord_flip()+ 
-  theme_gray(base_size = 15) + #grayテーマで
-  theme_gray(base_family = "HiraKakuPro-W3") #文字化けしないおまじない
+
 
 #男性と女性の身長の雨雲図を書いて比較してみて
-
-library(ggdist)
-library(tidyquant)
-
-ggplot(data = ten_kukan100) +     # tenkukan20データでキャンバス準備
-  aes(x = sex_c, y = height, fill = sex_c)+ # height,weight列をx,y軸にmapping,sexごとに色分け
-  ggdist::stat_halfeye(
-    adjust =0.5,
-    justification = -.2,
-    .width = 0,
-    point_colour = NA)+
-  # はこひげ図を描く
-  geom_boxplot(
-    width = 0.12,
-    outlier.color = NA,
-    alpha = 0.5) +
-  ggdist::stat_dots(
-    side = "left",
-    justification = 1.1,
-    binwidth = 0.25)+
-  xlab("sex") + ylab("height") +
-  scale_fill_tq()+
-  theme_tq()+
-  theme_gray(base_family = "HiraKakuPro-W3") +#文字化けしないおまじない
-  coord_flip()
 
